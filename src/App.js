80d8a6e1d70fb9a6e5ee490b9de70c2e9212a813
Priviledge <133637242+Privils@users.noreply.github.com>
2026-01-08 23:18:45 +0200

    redo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import './App.css';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
-import Services from './components/Services';
+// import Services from './components/Services';
 import Footer from './components/Footer';
 import Portfolio from './components/Portfolio';
 import 'animate.css';
 
 function App() {
-
   return (
-  <>
-  <Router>
-    <Header/>
-    <Routes>
-      <Route index element={<Home/>}/>
-      <Route path='/services' element={<Services/>} />
-      <Route path='/Portfolio' element={<Portfolio/>}/>
-    </Routes>
-    <Footer/>
-  </Router>
-  </>
+    <Router>
+      <Header />
+      <Routes> 
+        <Route index element={<Home />} />
+        {/* <Route path='/services' element={<Services />} /> */}
+        <Route path='/portfolio' element={<Portfolio />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
